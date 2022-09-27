5a795572a5f2a9279922de3844fb14fa78dbe9df
Elene Lelashvili <elenelelashvili6@gmail.com>
2022-09-27 10:55:46 +0400

    done

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    next();
+})
 server.use(middlewares);
 server.use(router);
 
